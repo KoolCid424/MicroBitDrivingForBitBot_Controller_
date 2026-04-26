c56be5eb658743a5ca392b2a1197171c4a5940f5
Daniel <151843800+KoolCid424@users.noreply.github.com>
2026-04-26 13:42:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.ScreenUp)) {
+        radio.sendNumber(0)
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.ScreenDown)) {
+        radio.sendNumber(1)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(3)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(4)
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . # # #
+        # . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(2000)
+})
